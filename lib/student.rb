d7531574a96c580aec41ba7de25fe7e7caa5b60e
wei chen <weichen9083@gmail.com>
2020-05-05 19:06:34 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,44 +3,46 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  #erase below 
   attr_accessor :name, :grade
-attr_reader :id
-def initialize(id = nil, name, grade)
-@name = name
-@grade = grade
-@id = id
-end
-
-def self.create_table
-  sql = <<-SQL
-  create table students(id integer primary key, name text, grade text);
-  SQL
-  DB[:conn].execute(sql)
-
-end
-
-def self.drop_table
-sql = <<-SQL
-drop table students;
-SQL
-DB[:conn].execute(sql)
-end
-
-def save
-  sql = <<-SQL
-  insert into students (name,grade) values(?,?);
-  SQL
-  DB[:conn].execute(sql, self.name, self.grade)
-  @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
-end
-
-def self.create(name:, grade:)
-student = Student.new(name, grade)
-student.save
-student
-end
-
-#erase above
+  attr_reader :id 
   
-end
+  
+  def initialize (name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id 
+  end 
+  
+  
+  def self.create_table 
+    quote = <<-q 
+    CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT, 
+    grade TEXT)
+    q
+    
+    DB[:conn].execute(quote)
+  end 
+  
+  def self.drop_table 
+    DB[:conn].execute("DROP TABLE IF EXISTS students")
+  end 
+  
+  def save
+    quote = <<-q 
+    INSERT INTO students (name, grade) VALUES (?,?) 
+    q
+    
+    DB[:conn].execute(quote, self.name , self.grade)
+    
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end 
+  
+  def self.create(name:, grade:)
+    what = Student.new(name, grade)
+    what.save 
+    what 
+  end 
+  
+end 
