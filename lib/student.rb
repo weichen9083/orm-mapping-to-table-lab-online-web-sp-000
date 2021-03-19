781e2144597434e295a906a21479089ebcb403d4
wei chen <weichen9083@gmail.com>
2021-03-19 03:03:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,40 +3,39 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  attr_accessor :name, :grade
-  attr_reader :id 
+  attr_accessor :name , :grade
+  attr_reader :id
   
-  
-  def initialize (name, grade, id=nil)
-    @name = name
-    @grade = grade
+  def initialize(name, grade, id = nil)
+    @name = name 
+    @grade = grade 
     @id = id 
   end 
   
-  
   def self.create_table 
-    quote = <<-q 
-    CREATE TABLE IF NOT EXISTS students (
-    id INTEGER PRIMARY KEY,
-    name TEXT, 
-    grade TEXT)
+    sql = <<-q 
+    CREATE TABLE IF NOT EXISTS students(id INTEGER PRIMARy KEY,
+    name TEXT,
+    grade INTEGER)
     q
-    
-    DB[:conn].execute(quote)
+    DB[:conn].execute(sql)
   end 
   
   def self.drop_table 
-    DB[:conn].execute("DROP TABLE IF EXISTS students")
+    sql = <<-q 
+    DROP TABLE students
+    q
+   DB[:conn].execute(sql) 
   end 
   
-  def save
+ def save
     quote = <<-q 
     INSERT INTO students (name, grade) VALUES (?,?) 
     q
     
     DB[:conn].execute(quote, self.name , self.grade)
     
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    @id = DB[:conn].execute("SELECT id FROM students order by id desc limit 1 ")[0][0]
   end 
   
   def self.create(name:, grade:)
@@ -44,5 +43,9 @@ class Student
     what.save 
     what 
   end 
+  
+  
+  
+  
   
 end 
